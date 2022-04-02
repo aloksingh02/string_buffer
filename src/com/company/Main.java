3b076a818024bcc9576be08e3c4333396bd05ea5
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-04-02 23:05:17 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuffer buffer=new StringBuffer("Alok");
+        buffer.append("singh");
+        System.out.println(buffer);
+	// write your code here
+    }
+}
